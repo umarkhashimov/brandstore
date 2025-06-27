b0a301481008c834a4423aa02592fbcc7883b60a
Umar Hoshimov <umarkhashimov05@gmail.com>
2025-06-27 11:27:48 +0500

    last commit

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import mainpage_view, category_view, detail_view, cart_view, write_comment_view
+from .views import mainpage_view, category_view, detail_view, cart_view, write_comment_view, search_view, email_subscribe_view, leave_contact_view
 
 app_name = 'main'
 
@@ -9,5 +9,8 @@ urlpatterns = [
     path('category/<int:id>', category_view, name='category'),
     path('detail/<int:id>', detail_view, name='detail'),
     path('cart/', cart_view, name='cart'),
-    path('new-comment/<int:id>', write_comment_view, name='write_comment')
+    path('new-comment/<int:id>', write_comment_view, name='write_comment'),
+    path('search/', search_view, name='search'),
+    path('email-subscribe/', email_subscribe_view, name='email-subscribe'),
+    path('contact/', leave_contact_view, name='leave-contact')
 ]
--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
 from .models import CommentsModel
 from cart.models import CartModel
+import requests
 # Create your views here.
 
 from products.models import ProdutsModel, TagsModel
@@ -83,12 +84,93 @@ def cart_view(request):
 
 def write_comment_view(request, id):
     product = get_object_or_404(ProdutsModel, id=id)
+    
+    if request.user.is_authenticated:
 
-    if request.method == 'POST':
-        text = request.POST.get('comment')
+        if request.method == 'POST':
+            text = request.POST.get('comment')
 
-        CommentsModel.objects.create(user=request.user, product=product, comment=text)
+            CommentsModel.objects.create(user=request.user, product=product, comment=text)
 
 
     return redirect('main:detail', id=product.id)
 
+
+def search_view(request):
+    q = request.GET.get('q')
+    show = request.GET.get('show')
+    sort = request.GET.get('sort', None)
+
+    products = ProdutsModel.objects.all()
+
+    if q:
+        products = products.filter(name__contains=q)
+
+    if sort:
+        if sort == '1':
+            products = products.order_by('price')
+        elif sort == '2':
+            products = products.order_by('-price')
+        elif sort == '3':
+            products = products.order_by('name')
+
+    paginate_by = 16
+    if show:
+        paginate_by = int(show)
+
+    paginator = Paginator(products, paginate_by) 
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj       
+    }
+
+    return render(request, 'category.html', context)
+
+
+
+def email_subscribe_view(request):
+    email = request.GET.get('email')
+
+    bot_token = '7599058591:AAG6un9TXifXsRtthKmb3wvbK-wiNocY7ak'
+    user_id = 7154980539
+    message = f'Новая подписка: \n{email}'
+    
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+
+    context = {
+        'chat_id': user_id,
+        'text':message 
+    }
+
+    response = requests.post(url, data=context)
+
+
+    next_url = request.GET.get('next')
+
+    return redirect(next_url) 
+
+
+def leave_contact_view(request):
+    full_name = request.POST.get('full_name') 
+    email = request.POST.get('email') 
+    phone = request.POST.get('phone') 
+    note = request.POST.get('note') 
+
+
+    bot_token = '7599058591:AAG6un9TXifXsRtthKmb3wvbK-wiNocY7ak'
+    user_id = 7154980539
+    message = f'Контакт: \nИмя: {full_name}\nEmail: {email}\nPhone: {phone}\nNote: {note}'
+    
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+
+    context = {
+        'chat_id': user_id,
+        'text':message 
+    }
+
+    response = requests.post(url, data=context)
+
+    return redirect('main:home')
